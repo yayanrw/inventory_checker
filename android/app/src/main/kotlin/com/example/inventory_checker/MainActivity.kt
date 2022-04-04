dee9ed280d98152bf91ee83e8a0ef543f588016d
yayanrw <yayanraw@gmail.com>
2022-04-04 11:42:31 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/inventory_checker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.inventory_checker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/config/apps_config.dart
@@ -0,0 +1,3 @@
+class AppsConfig {
+  static const appTitle = 'Inventory Checker';
+}
