95597aec8eb06ca2b80b71e0cfdcebbd98516b62
yayanrw <yayanraw@gmail.com>
2022-04-07 23:30:34 +0700

    change defaul padding

--- lib/core/config/apps_config.dart
+++ lib/core/config/apps_config.dart
@@ -4,7 +4,7 @@ class AppsConfig {
   static const appTitle = 'Inventory Checker';
   static final defaultTextColor = MyColors.textBlack;
   static const defaultRadius = 8.0;
-  static const defaultPadding = 16.0;
+  static const defaultPadding = 8.0;
   static const baseUrl = 'http://103.157.96.97/codeigniter3-jwtauth/';
   static const fontFamily = 'Popins';
 }
