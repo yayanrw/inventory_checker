f7e9e3d59cda56e48aeb41f1142a5ae4ae724c5e
yayanrw <yayanraw@gmail.com>
2022-04-08 00:51:43 +0700

    add router

--- lib/core/routes/router.dart
+++ lib/core/routes/router.dart
@@ -1,9 +1,11 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:inventory_checker/core/routes/auth_guard.dart';
+import 'package:inventory_checker/features/check_qr/presentation/pages/check_qr_page.dart';
 import 'package:inventory_checker/features/home/presentation/pages/home_page.dart';
 import 'package:inventory_checker/features/login/presentation/pages/login_page.dart';
 import 'package:inventory_checker/features/others/presentation/pages/not_found_page.dart';
 import 'package:inventory_checker/features/others/presentation/pages/under_development_page.dart';
+import 'package:inventory_checker/features/scan_out/presentation/pages/scan_out_page.dart';
 
 @MaterialAutoRouter(
   replaceInRouteName: 'Page,Route',
@@ -21,8 +23,9 @@ import 'package:inventory_checker/features/others/presentation/pages/under_devel
       path: '/not-found',
       page: NotFoundPage,
     ),
-    AutoRoute(
-        path: '/home-page', page: HomePage, guards: [AuthGuard], children: []),
+    AutoRoute(path: '/home-page', page: HomePage, guards: [AuthGuard]),
+    AutoRoute(path: '/check-qr-page', page: CheckQrPage, guards: [AuthGuard]),
+    AutoRoute(path: '/scan-out-page', page: ScanOutPage, guards: [AuthGuard])
   ],
 )
 class $AppRouter {}
--- lib/core/routes/router.gr.dart
+++ lib/core/routes/router.gr.dart
@@ -10,56 +10,71 @@
 //
 // ignore_for_file: type=lint
 
-import 'package:auto_route/auto_route.dart' as _i5;
-import 'package:flutter/material.dart' as _i6;
+import 'package:auto_route/auto_route.dart' as _i7;
+import 'package:flutter/material.dart' as _i8;
 
+import '../../features/check_qr/presentation/pages/check_qr_page.dart' as _i5;
 import '../../features/home/presentation/pages/home_page.dart' as _i4;
 import '../../features/login/presentation/pages/login_page.dart' as _i1;
 import '../../features/others/presentation/pages/not_found_page.dart' as _i3;
 import '../../features/others/presentation/pages/under_development_page.dart'
     as _i2;
-import 'auth_guard.dart' as _i7;
+import '../../features/scan_out/presentation/pages/scan_out_page.dart' as _i6;
+import 'auth_guard.dart' as _i9;
 
-class AppRouter extends _i5.RootStackRouter {
+class AppRouter extends _i7.RootStackRouter {
   AppRouter(
-      {_i6.GlobalKey<_i6.NavigatorState>? navigatorKey,
+      {_i8.GlobalKey<_i8.NavigatorState>? navigatorKey,
       required this.authGuard})
       : super(navigatorKey);
 
-  final _i7.AuthGuard authGuard;
+  final _i9.AuthGuard authGuard;
 
   @override
-  final Map<String, _i5.PageFactory> pagesMap = {
+  final Map<String, _i7.PageFactory> pagesMap = {
     LoginRoute.name: (routeData) {
-      return _i5.MaterialPageX<dynamic>(
+      return _i7.MaterialPageX<dynamic>(
           routeData: routeData, child: const _i1.LoginPage());
     },
     UnderDevelopmentRoute.name: (routeData) {
-      return _i5.MaterialPageX<dynamic>(
+      return _i7.MaterialPageX<dynamic>(
           routeData: routeData, child: const _i2.UnderDevelopmentPage());
     },
     NotFoundRoute.name: (routeData) {
-      return _i5.MaterialPageX<dynamic>(
+      return _i7.MaterialPageX<dynamic>(
           routeData: routeData, child: const _i3.NotFoundPage());
     },
     HomeRoute.name: (routeData) {
-      return _i5.MaterialPageX<dynamic>(
+      return _i7.MaterialPageX<dynamic>(
           routeData: routeData, child: const _i4.HomePage());
+    },
+    CheckQrRoute.name: (routeData) {
+      return _i7.MaterialPageX<dynamic>(
+          routeData: routeData, child: const _i5.CheckQrPage());
+    },
+    ScanOutRoute.name: (routeData) {
+      return _i7.MaterialPageX<dynamic>(
+          routeData: routeData, child: const _i6.ScanOutPage());
     }
   };
 
   @override
-  List<_i5.RouteConfig> get routes => [
-        _i5.RouteConfig(LoginRoute.name, path: '/'),
-        _i5.RouteConfig(UnderDevelopmentRoute.name, path: '/under-development'),
-        _i5.RouteConfig(NotFoundRoute.name, path: '/not-found'),
-        _i5.RouteConfig(HomeRoute.name, path: '/home-page', guards: [authGuard])
+  List<_i7.RouteConfig> get routes => [
+        _i7.RouteConfig(LoginRoute.name, path: '/'),
+        _i7.RouteConfig(UnderDevelopmentRoute.name, path: '/under-development'),
+        _i7.RouteConfig(NotFoundRoute.name, path: '/not-found'),
+        _i7.RouteConfig(HomeRoute.name,
+            path: '/home-page', guards: [authGuard]),
+        _i7.RouteConfig(CheckQrRoute.name,
+            path: '/check-qr-page', guards: [authGuard]),
+        _i7.RouteConfig(ScanOutRoute.name,
+            path: '/scan-out-page', guards: [authGuard])
       ];
 }
 
 /// generated route for
 /// [_i1.LoginPage]
-class LoginRoute extends _i5.PageRouteInfo<void> {
+class LoginRoute extends _i7.PageRouteInfo<void> {
   const LoginRoute() : super(LoginRoute.name, path: '/');
 
   static const String name = 'LoginRoute';
@@ -67,7 +82,7 @@ class LoginRoute extends _i5.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i2.UnderDevelopmentPage]
-class UnderDevelopmentRoute extends _i5.PageRouteInfo<void> {
+class UnderDevelopmentRoute extends _i7.PageRouteInfo<void> {
   const UnderDevelopmentRoute()
       : super(UnderDevelopmentRoute.name, path: '/under-development');
 
@@ -76,7 +91,7 @@ class UnderDevelopmentRoute extends _i5.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i3.NotFoundPage]
-class NotFoundRoute extends _i5.PageRouteInfo<void> {
+class NotFoundRoute extends _i7.PageRouteInfo<void> {
   const NotFoundRoute() : super(NotFoundRoute.name, path: '/not-found');
 
   static const String name = 'NotFoundRoute';
@@ -84,8 +99,24 @@ class NotFoundRoute extends _i5.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i4.HomePage]
-class HomeRoute extends _i5.PageRouteInfo<void> {
+class HomeRoute extends _i7.PageRouteInfo<void> {
   const HomeRoute() : super(HomeRoute.name, path: '/home-page');
 
   static const String name = 'HomeRoute';
+}
+
+/// generated route for
+/// [_i5.CheckQrPage]
+class CheckQrRoute extends _i7.PageRouteInfo<void> {
+  const CheckQrRoute() : super(CheckQrRoute.name, path: '/check-qr-page');
+
+  static const String name = 'CheckQrRoute';
+}
+
+/// generated route for
+/// [_i6.ScanOutPage]
+class ScanOutRoute extends _i7.PageRouteInfo<void> {
+  const ScanOutRoute() : super(ScanOutRoute.name, path: '/scan-out-page');
+
+  static const String name = 'ScanOutRoute';
 }
